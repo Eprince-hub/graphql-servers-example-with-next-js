10e442c9bfce2cbe87c76c49ec8d6425c5608b0f
Victor Ejike Nwosu <ejikello123@yahoo.com>
2023-02-19 15:28:41 +0100

    Add login frontend and display animal in layout page

---
+++ app/(Auth)/login/page.tsx
@@ -0,0 +1,19 @@
+import ApolloClientProvider from '@/app/ApolloClientProvider';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+import LoginForm from './LoginForm';
+
+export default function page() {
+  const nextCookies = cookies();
+
+  const fakeSessionToken = nextCookies.get('fakeSessionToken');
+
+  if (fakeSessionToken?.value) {
+    redirect('/');
+  }
+  return (
+    <ApolloClientProvider initialApolloState={JSON.stringify([])}>
+      <LoginForm />
+    </ApolloClientProvider>
+  );
+}
