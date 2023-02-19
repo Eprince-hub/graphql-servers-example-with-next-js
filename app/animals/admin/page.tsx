312dba2270a19eaa346cb2180bc3882f48e71692
Victor Ejike Nwosu <ejikello123@yahoo.com>
2023-02-19 15:59:33 +0100

    Prevent none admin users from admin page

--- app/animals/admin/page.tsx
+++ app/animals/admin/page.tsx
@@ -1,9 +1,35 @@
 import ApolloClientProvider from '@/app/ApolloClientProvider';
+import { initializeApollo } from '@/util/client';
+import { gql } from '@apollo/client';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 import AdminDashboard from './AdminDashboard';
 
-export default function AnimalsAdminPage() {
+export default async function AnimalsAdminPage() {
+  const client = initializeApollo(null);
+  const nextCookies = cookies();
+
+  const fakeSessionToken = nextCookies.get('fakeSessionToken');
+
+  const { data } = await client.query({
+    query: gql`
+      query GetLoggedInAnimalByFirstName($firstName: String! = ${fakeSessionToken?.value}) {
+        getLoggedInAnimalByFirstName(firstName: $firstName) {
+          accessory
+          firstName
+        }
+      }
+    `,
+  });
+
+  if (!data.getLoggedInAnimalByFirstName) {
+    redirect('/login');
+  }
+
   return (
-    <ApolloClientProvider initialApolloState={JSON.stringify([])}>
+    <ApolloClientProvider
+      initialApolloState={JSON.stringify(client.cache.extract())}
+    >
       <AdminDashboard />
     </ApolloClientProvider>
   );
