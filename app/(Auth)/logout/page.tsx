37c6ea22e56953d69eac39a6e41d946194057b07
Victor Ejike Nwosu <ejikello123@yahoo.com>
2023-02-20 21:19:04 +0100

    Add complete logout code

--- app/(Auth)/logout/page.tsx
+++ app/(Auth)/logout/page.tsx
@@ -1,5 +1,12 @@
+import { headers } from 'next/headers';
 import { redirect } from 'next/navigation';
 
 export default function LogoutPage() {
+  const sessionToken = headers().get('x-sessionToken-to-delete');
+
+  if (sessionToken) {
+    // FIXME: Delete session from `sessions` database table for real
+    console.log(sessionToken);
+  }
   redirect('/');
 }
